b7c70bfbf9de856efbae83522cc94e668f1aa11f
Ashkan Taravati <ashkan.taravati@gmail.com>
2018-04-04 17:37:14 +0430

    2 basic models __str__ persian verbose

--- main/models.py
+++ main/models.py
@@ -1,10 +1,31 @@
 from django.db import models
+#CHOICES
+SEMESTER_CHOICES=[(1,u'پاییز'),(2,u'بهار'),(3,u'تابستان')]
+
+
+
+
+
 
 # Create your models here.
+#مکان دفاع
 class DefensePlace(models.Model):
-   PlaceName=models.CharField(max_length=50)
-   RoomName=models.CharField(max_length=50)
-   BuildingName=models.CharField(max_length=50)
-
-   def __unicode__(self):
-       return "{}-{}-{}".format(self.PlaceName,self.BuildingName,self.RoomName)
+    placeName=models.CharField(max_length=50,verbose_name='نام مستعار اتاق دفاع')
+    roomName=models.CharField(max_length=50,verbose_name='نام یا شماره اتاق')
+    buildingName=models.CharField(max_length=50,verbose_name='نام ساختمان')
+    semester=models.ForeignKey('Semester',verbose_name='نیمسال تحصیلی')
+    class Meta:
+        verbose_name=u'محل دفاع'
+        verbose_name_plural=u'محل‌های دفاع'
+    def __str__(self):
+        return "{}-{}-{}".format(self.placeName,self.buildingName,self.roomName)
+#نیمسال تحصیلی
+class Semester(models.Model):
+    beginningYear=models.IntegerField(verbose_name='سال شروع')
+    endingYear=models.IntegerField(verbose_name='سال پایان')
+    semesterType=models.IntegerField(choices=SEMESTER_CHOICES,verbose_name='نوع نیمسال')
+    class Meta:
+        verbose_name=u'نیمسال تحصیلی'
+        verbose_name_plural=u'نیمسال‌های تحصیلی'        
+    def __str__(self):
+        return u"{} {}-{}".format(self.get_semesterType_display(),self.beginningYear,self.endingYear)
