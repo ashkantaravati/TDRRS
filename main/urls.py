8be9a5303f47b9b5c254820fd9aa00211710e754
Ashkan Taravati <ashkan.taravati@gmail.com>
2018-04-28 00:39:24 +0430

    changepassword init

--- main/urls.py
+++ main/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^login$',views.doLogin,name='login'),
     url(r'^DefenseTimes',views.readAllDefenseTimes,name='DefenseTimes'),
     url(r'^myRequests',views.myRequests,name='MyRequests'),    
-    url(r'^logout$',views.doLogout,name='logout')
+    url(r'^logout$',views.doLogout,name='logout'),
+    url(r'^changePassword',views.changePassword,name='changePassword'),        
 ]
--- main/views.py
+++ main/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate,logout,login
+from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
 from django.http import HttpResponse,HttpResponseRedirect
 from django.template.loader import get_template
 import datetime
 from .models import DefenseTime
-from django.contrib.auth import authenticate,logout,login
 
 # Create your views here.
 def index(request):
@@ -41,6 +44,41 @@ def doLogin(request):
             return HttpResponseRedirect('Dashboard')
             
         else:
-            return HttpResponse('failed')
+            return render(request,'login.html')
 def myRequests(request):
     return render(request, 'myRequests.html')
+
+def changePassword(request):
+    request.session['result']=''
+    if request.method=='GET':
+        request.session['result']=''    
+        return render(request,'changePassword.html')
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('changePassword')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'changePassword.html', {
+        'form': form
+    })
+    # elif request.method=='POST':
+    #     oldPasswd=request.POST['oldPasswd']
+    #     confPasswd=request.POST['confPasswd']
+    #     newPasswd=request.POST['newPasswd']
+    #     if oldPasswd==confPasswd:
+    #         currentUser=request.user
+    #         if currentUser.password==oldPasswd:
+    #             currentUser.password=newPasswd
+    #             currentUser.save()
+    #             request.session['result']='با موفقیت انجام شد'
+    #         else:
+    #             request.session['result']='اطلاعات وارد شده صحیح نمی‌باشد'
+    #     else:
+    #         request.session['result']='تکرار گذرواژه درست نیست'       
+    #     return render(request,'changePassword.html')
